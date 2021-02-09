dfe1b37ebf4eb3b039e21c8438428162dfefe7e9
Bharath Kumar R <bharath2668@gmail.com>
2021-02-09 21:22:47 +0530

    Commit and Push for n number of times

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
+const random = require('random')
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const makeCommit = (n) => {
+   if (n === 0) return simpleGit().push(); // push after n successful commits 
 
-const data = {
-   date: DATE
+   const x = random.int(0, 54);
+   const y = random.int(0, 6);
+
+   const DATE = moment()
+      .subtract(1, "y") // no of year back from current date
+      .add(1, "d") // matrix(0,0)
+      .add(x, "w") // week column 
+      .add(y, "d") // days row
+      .format();
+
+   const data = {
+      date: DATE,
+   };
+   console.log(DATE);
+   jsonfile.writeFile(FILE_PATH, data, () => { // calbk fn
+      simpleGit().add([FILE_PATH])
+         .commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n))
+         .push();
+   });
 }
 
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push();
+makeCommit(2);
